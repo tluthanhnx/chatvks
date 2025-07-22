35f78fa442a3ab8b78ad69ccd28beb6d0d6cbfc2
tluthanhnx <thanhnx@thanglong.edu.vn>
2025-07-22 22:47:40 +0700

    Update alias_mapping.py

--- alias_mapping.py
+++ alias_mapping.py
@@ -1,5 +1,5 @@
 alias_mapping = {
-    "vụ án": "spp_case",
+    "vụ án": "Select * from spp_case where CASENAME='Tên vụ án'",
     "bị can": "spp_accused",
     "tội danh bị can": "spp_accusedlaw",
     "kháng nghị": "spp_against",
